1b2f6e4832241a4ef6ceb5e864f1e86cd4c4f208
Jenny K <jkam17@gmail.com>
2018-08-21 23:22:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def bath_badge_maker(array)
+  badges=array.collect{|x| badge_maker(x)}
+  
+end
+
+def assign_rooms(array)
+  assignments=array.collect{|name| puts "Hello #{name}! You'll be assigned to room #{array.index(name)}!"}
+end
+  
+def printer(array)
+  array.each
+  
+  
